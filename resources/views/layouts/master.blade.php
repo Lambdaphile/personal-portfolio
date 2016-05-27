f7c13f16b1c5d2ea7c7bc75afda1841419a01d5b
Enkido <malyintoevis@gmail.com>
2016-05-27 15:58:57 +0300

    Boostrap improvments and interation are maden.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -18,12 +18,6 @@
     <!-- Costum fonts -->
     <link href='https://fonts.googleapis.com/css?family=Yanone+Kaffeesatz' rel='stylesheet' type='text/css'>
     
-    <!-- Bootstrap core CSS -->
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-    
     <!-- Costum stylesheets -->
     <link rel="stylesheet" type="text/css" href="/css/app.css">
     
