45149d1d7b4467a8ee01fee54e43fcaac95c07dd
Enkido <malyintoevis@gmail.com>
2016-07-27 00:19:47 +0300

    27/07/2016:
    - Projects section is done.
    - HTML & CSS is improved.
    - Not sure if this commit message is that useful. :squirrel:

--- resources/views/pages/sketch.blade.php
+++ resources/views/pages/sketch.blade.php
@@ -7,23 +7,84 @@
   <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
   <title>Sketching</title>
+
+  <!-- Costum fonts -->
+  <link href='https://fonts.googleapis.com/css?family=Lato|Raleway|Poiret+One' rel='stylesheet' type='text/css'>
   
   <!-- Costum CSS -->
   <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-  <section>
+  
+  <section id="sketch-section">
     <div class="container-fluid">
       <div class="row">
-        <div style="background-color: blue;" class="col-md-6">
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        <div class="col-xs-12 skills-section-offset">
+          <h2 class="section-name">Projects</h2>
+          <!-- /.section-name -->
         </div>
-        <div style="background-color: red;" class="col-md-6">
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-        </div>
+        <!-- /.col-xs-12 skills-section-offset -->
       </div>
+      <!-- /.row -->
     </div>
+    <!-- /.container-fluid -->
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-4 skills-section-offset minor-projects">
+          <img class="project-pic-full" src="/images/portfolio-capture.png" alt="portfolio-pic"/>
+          <div class="project-description">
+            <h2>This Website</h2>
+            <div class="small-line"></div>
+            <!-- /.small-line -->
+            <p>My current personal resume and portfolio website.</p>
+          </div>
+          <!-- /.project-description -->
+          <div class="used-technologies">
+            <h3>PHP</h3>
+            <h3>Laravel</h3>
+            <h3>Responsive</h3>
+            <h3>HTML5</h3>
+            <h3>CSS3</h3>
+            <h3>Sass</h3>
+            <h3>Bootstrap</h3>
+            <h3>JQuery</h3>
+            <p>July 2016</p>
+          </div>
+          <!-- /.used-technologies -->
+        </div>
+        <!-- /.col-sm-4 skills-section-offset minor-projects minor-projects -->
+        <div class="col-sm-4">
+          <img class="project-pic-full" src="https://alyssasfu.files.wordpress.com/2015/03/screen-shot-2015-03-21-at-11-01-39-am.png" alt="riot-game-api-app-pic"/>
+          <div class="project-description">
+            <h2>Meta Headz</h2>
+            <div class="small-line"></div>
+            <!-- /.small-line -->
+            <p>A League Of Legends app for tracking pro players builds, masteries and runes using Riot Games API.</p>
+          </div>
+          <!-- /.project-description -->
+          <div class="used-technologies">
+            <h3>PHP</h3>
+            <h3>Laravel</h3>
+            <h3>JavaScript</h3>
+            <h3>CoffeeScript</h3>
+            <h3>JQuery</h3>
+            <h3>Responsive</h3>
+            <h3>HTML5</h3>
+            <h3>CSS3</h3>
+            <h3>Sass</h3>
+            <h3>Bootstrap</h3>
+            <p>July 2016</p>
+          </div>
+          <!-- /.used-technologies -->
+        </div>
+        <!-- /.col-sm-4 -->
+      </div>
+      <!-- /.row -->
+    </div>
+    <!-- /.container fluid -->
   </section>
+  <!-- /.sketch -->
+
   <!-- Latest compressed, production jQuery 2.2.3 -->
   <script src="https://code.jquery.com/jquery-2.2.3.min.js"></script>
   <!-- Latest compiled and minified JavaScript -->
